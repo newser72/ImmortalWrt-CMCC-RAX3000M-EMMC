96d6f36ab7332315db32b28119071d0dc2c0e86a
newser72 <107610522+newser72@users.noreply.github.com>
2026-08-12 00:53:17 +0800

    Create diy-part3.sh

---
+++ scripts/24.10-6.6/diy-part3.sh
@@ -0,0 +1,21 @@
+#!/bin/bash
+#
+# 版权所有 (c) 2019-2020 P3TERX <https://p3terx.com>
+#
+# 这是一个自由软件，根据 MIT 许可证授权。
+# 详细信息请参见 /LICENSE。
+#
+# https://github.com/P3TERX/Actions-OpenWrt
+
+# 修改默认 IP
+CONFIG_FILE="package/base-files/files/bin/config_generate"
+if [ -f "$CONFIG_FILE" ]; then
+  if ! grep -q "192.168.6.1" "$CONFIG_FILE"; then
+    sed -i 's/192\.168\.6\.1/192.168.6.1/g; s/192\.168\.1\.1/192.168.6.1/g' "$CONFIG_FILE"
+    echo "IP 地址已更新为 192.168.6.1"
+  else
+    echo "IP 地址已是 192.168.6.1，无需修改"
+  fi
+else
+  echo "警告：$CONFIG_FILE 不存在，跳过 IP 修改"
+fi
