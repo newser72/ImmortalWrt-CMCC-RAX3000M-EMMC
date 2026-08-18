c90aeadeaf5b0fe1c8875224b6efa71fa738f9fa
newser72 <107610522+newser72@users.noreply.github.com>
2026-08-18 10:07:50 +0800

    Create diy_part4.sh

---
+++ scripts/24.10-6.6/diy_part4.sh
@@ -0,0 +1,52 @@
+#!/bin/bash
+#
+# 版权所有 (c) 2019-2020 P3TERX <https://p3terx.com>
+#
+# 这是一个自由软件，根据 MIT 许可证授权。
+# 详细信息请参见 /LICENSE。
+#
+# https://github.com/P3TERX/Actions-OpenWrt
+
+# 修改默认 IP
+CONFIG_FILE="package/base-files/files/bin/config_generate"
+if [ -f "$CONFIG_FILE" ]; then
+  if ! grep -q "192.168.6.1" "$CONFIG_FILE"; then
+    sed -i 's/192\.168\.6\.1/192.168.6.1/g; s/192\.168\.1\.1/192.168.6.1/g' "$CONFIG_FILE"
+    echo "IP 地址已更新为 192.168.6.1"
+  else
+    echo "IP 地址已是 192.168.6.1，无需修改"
+  fi
+else
+  echo "警告：$CONFIG_FILE 不存在，跳过 IP 修改"
+fi
+
+# 创建default目录，请替换nx30pro高功率固件
+mkdir package/mtk/drivers/mt_wifi/files/mt7981-default-eeprom
+#cp /home/runner/work/rax3000m-emcc-immortalwrt24.10-new/rax3000m-emcc-immortalwrt24.10-new/scripts/24.10-6.6/nx30pro_eeprom.bin package/mtk/drivers/mt_wifi/files/mt7981-default-eeprom/MT7981_iPAiLNA_EEPROM.bin
+cp $GITHUB_WORKSPACE/scripts/24.10-6.6/nx30pro_eeprom.bin package/mtk/drivers/mt_wifi/files/mt7981-default-eeprom/MT7981_iPAiLNA_EEPROM.bin
+rm -f target/linux/mediatek/image/filogic.mk;cp $GITHUB_WORKSPACE/scripts/24.10-6.6/filogic.mk target/linux/mediatek/image/filogic.mk
+
+#针对25.12版本，补上缺少的rax3000m的dts文件
+#cp $GITHUB_WORKSPACE/scripts/24.10-6.6/mt7981b-cmcc-rax3000m-emmc.dts target/linux/mediatek/dts/
+#cp $GITHUB_WORKSPACE/scripts/24.10-6.6/mt7981b-cmcc-rax3000m-nand.dts target/linux/mediatek/dts/
+
+# 删除 package/mtk/drivers/mt_wifi/files/mt7981-default-eeprom/e2p
+rm -f package/mtk/drivers/mt_wifi/files/mt7981-default-eeprom/e2p
+if [ $? -eq 0 ]; then
+  echo "已删除 package/mtk/drivers/mt_wifi/files/mt7981-default-eeprom/e2p"
+else
+  echo "错误：删除 package/mtk/drivers/mt_wifi/files/mt7981-default-eeprom/e2p 失败"
+fi
+
+# 创建 MT7981 固件符号链接
+EEPROM_FILE="package/mtk/drivers/mt_wifi/files/mt7981-default-eeprom/MT7981_iPAiLNA_EEPROM.bin"
+if [ -f "$EEPROM_FILE" ]; then
+  mkdir -p files/lib/firmware
+  cp $GITHUB_WORKSPACE/scripts/24.10-6.6/26dB-justfor25.12-MT7981_iPAiLNA_EEPROM.bin files/lib/firmware/MT7981_iPAiLNA_EEPROM.bin
+  ln -sf /lib/firmware/MT7981_iPAiLNA_EEPROM.bin files/lib/firmware/e2p
+  echo "符号链接已创建"
+  ls -l files/lib/firmware/e2p || { echo "错误：符号链接创建失败"; exit 1; }
+else
+  echo "错误：$EEPROM_FILE 不存在，无法创建符号链接"
+  exit 1
+fi
