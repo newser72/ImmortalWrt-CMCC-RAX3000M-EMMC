c046fd46cff6b6519f76563801c6a7bc2a50affb
newser72 <107610522+newser72@users.noreply.github.com>
2026-08-20 00:31:14 +0800

    Update diy-part4.sh

--- scripts/24.10-6.6/diy-part4.sh
+++ scripts/24.10-6.6/diy-part4.sh
@@ -22,8 +22,8 @@ fi
 
 # 创建default目录，请替换nx30pro高功率固件
 mkdir package/mtk/drivers/mt_wifi/files/mt7981-default-eeprom
-#cp /home/runner/work/rax3000m-emcc-immortalwrt24.10-new/rax3000m-emcc-immortalwrt24.10-new/scripts/24.10-6.6/nx30pro_eeprom.bin package/mtk/drivers/mt_wifi/files/mt7981-default-eeprom/MT7981_iPAiLNA_EEPROM.bin
-cp $GITHUB_WORKSPACE/scripts/24.10-6.6/nx30pro_eeprom.bin package/mtk/drivers/mt_wifi/files/mt7981-default-eeprom/MT7981_iPAiLNA_EEPROM.bin
+#cp /home/runner/work/rax3000m-emcc-immortalwrt24.10-new/rax3000m-emcc-immortalwrt24.10-new/scripts/24.10-6.6/25dB-nx30pro_eeprom.bin package/mtk/drivers/mt_wifi/files/mt7981-default-eeprom/MT7981_iPAiLNA_EEPROM.bin
+cp $GITHUB_WORKSPACE/scripts/24.10-6.6/25dB-nx30pro_eeprom.bin package/mtk/drivers/mt_wifi/files/mt7981-default-eeprom/MT7981_iPAiLNA_EEPROM.bin
 rm -f target/linux/mediatek/image/filogic.mk;cp $GITHUB_WORKSPACE/scripts/24.10-6.6/filogic.mk target/linux/mediatek/image/filogic.mk
 
 #针对25.12版本，补上缺少的rax3000m的dts文件
